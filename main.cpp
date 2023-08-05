a3b0f5db1ad78da8d082e35dd72192cf5c5fcba3
micielkirsten <micielkirsten@gmail.com>
2023-08-04 23:52:28 -0400

    added quick sort

--- main.cpp
+++ main.cpp
@@ -144,6 +144,30 @@ void mergeSortCities(std::vector<DataPoint>& data, int left, int right) {
     }
 }
 
+int partition(std::vector<DataPoint>& data, int low, int high) {
+    double pivot = data[high].avgTemp;
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (data[j].avgTemp <= pivot) {
+            i++;
+            std::swap(data[i], data[j]);
+        }
+    }
+    std::swap(data[i + 1], data[high]);
+    return i + 1;
+}
+
+void quickSort(std::vector<DataPoint> &data, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(data, low, high);
+        quickSort(data, low, pi - 1);
+        quickSort(data, pi + 1, high);
+    }
+}
+
 int main(){
     using namespace matplot;
 
@@ -187,6 +211,7 @@ int main(){
     }
 
     HeatMap(lat, lon, weight);
+    quickSort(data, 0, data.size() - 1);
 
     return 0;
 }
